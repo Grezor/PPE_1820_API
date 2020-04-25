611efc52592253eb41031820002df830091e2da0
Geoffrey Duplessi <grezor.gamer@gmail.com>
2020-04-25 08:20:19 +0100

    clean, add readme

--- bornes.php
+++ bornes.php
@@ -1,6 +1,7 @@
-<?php 
-header("Content*Type: application/json");
-require_once('db.php');
+<?php
+header("Content-Type: application/json");
+
+require_once 'db.php';
 
 $bornes = getAllBornes();
 $json = json_encode($bornes);
