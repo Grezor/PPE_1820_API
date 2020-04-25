af9efc3131ba546715893836cc2fdd16bfed3d58
Geoffrey Duplessi <grezor.gamer@gmail.com>
2020-04-25 20:17:58 +0100

    refacto toggleLikePhoto

--- db.php
+++ db.php
@@ -15,15 +15,22 @@ function getPDO() {
     die("ERREUR");
   }
 }
+
 /**
- * Affiches toutes les bornes
+ * Affiche le nombres de bornes
+ * @return array
  */
-function getAllBornes(){
-  $statement = getPDO()->prepare("SELECT * FROM bornes");
-  $statement->execute();
-  return $statement->fetchAll(PDO::FETCH_ASSOC);
+function getAllBornes()
+{
+    $statement = getPDO()->prepare("SELECT * FROM bornes");
+    $statement->execute();
+    return $statement->fetchAll(PDO::FETCH_ASSOC);
 }
 
+/**
+ * @param $code
+ * @return array
+ */
 function getPhotos($code){
   // $req = "SELECT photos.id, estAime FROM photos
   // JOIN reservations ON reservations.id = photos.id_reservation
@@ -59,24 +66,38 @@ function getUserIdWithToken($token){
   return $ids[0]["id_user"];
 }
 
-function toggleLikePhoto($photoId, $userId){
-  $req = "SELECT id_user 
-          FROM user_likes 
-          WHERE id_user = :id_user AND id_photo = :id_photo";
+/**
+ * @param $photoId
+ * @param $userId
+ * @return bool
+ */
+function toggleLikePhoto($photoId, $token){
+  $req = "SELECT id_user FROM user_token WHERE token = :token";
+  $statement = getPDO()->prepare($req);
+  $statement->execute([":token" => $token]);
+  $ids = $statement->fetchAll(PDO::FETCH_ASSOC);
+  // si on retrouve pas exactements un ligne, on arrete
+  if (count($ids) != 1) {
+    return false;
+  }
+  $userId = $ids[0]["id_user"];
+  // ajouter ou supprimer une ligne dans la table use_likes
+  // si l'utilisateur avais like, on supprime la ligne
+  // si l'utilisateur n'avais pas liké, on crée la ligne
+  $req = "SELECT id_user FROM user_token WHERE id_user = :id_user AND id_photo = :id_photo";
   $statement = getPDO()->prepare($req);
   $statement->execute([":id_user" => $userId, ":id_photo" => $photoId]);
   $ids = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-  // si je retourne plus d'un résultat c'est un problème
+  // si je retourn  plus de 1 résultat c'est un problème
   if (count($ids) > 1) {
     return false;
-  }
-  // si count($ids) == 0, on fait le INSERT sinon on fait le DELETE
-  $req = count($ids) == 0 ?
-          "INSERT INTO user_likes (id_user, id_photo) VALUES (:id_user, :id_photo)"
-          :"DELETE FROM user_likes WHERE id_user = :id_user AND id_photo = :id_photo";
+  } 
+  // si le test est vrai insert sinon DELETE
+  $req = count($ids) == 0 ? 
+        "INSERT into user_likes (id_user, id_photo) VALUES (:id_user, :id_photo)" 
+        : "DELETE FROM user_likes WHERE id_user = :id_user AND id_photo = :id_photo";
   $statement = getPDO()->prepare($req);
-  return $statement->execute([":id_user" => $userId, ":id_photo" => $photoId]);
+  return $statement->execute([":id_user" => $userId, ":id_photo" => $photoId]); // rreturn true si ca echoue
 }
 
 /**
@@ -92,6 +113,11 @@ function generateRandomString($length = 10) {
   return $randomString;
 }
 
+/**
+ * @param $login
+ * @param $password
+ * @return bool|string
+ */
 function getToken($login, $password){
   // trouver l'utilisateur en bdd
   $req = "SELECT id FROM users WHERE nickname = :login AND password = :password";
--- like.php
+++ like.php
@@ -1,15 +1,16 @@
 <?php 
 require_once 'db.php';
+
 $photoId = null; 
 $token = null;
-
+// récupération id de la photo
 if (isset($_POST["photo_id"])) {
   $photoId = $_POST["photo_id"];
 }else {
   http_response_code(400);
   return;
 }
-
+// récupération du token
 if (isset($_POST["token"])) {
   $token = $_POST["token"];
 }else{
@@ -23,7 +24,7 @@ if ($userId === false) {
   http_response_code(401);
   return;
 }
-
+$result = toggleLikePhoto($photoId, $token);
 $result = toggleLikePhoto($photoId, $userId);
 
 if ($result === false) {
